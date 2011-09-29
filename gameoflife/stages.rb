b5d9e8747dc39233d96db231d21e20a7b0d4a784
Justin Martin <lupusdei108@gmail.com>
2011-09-29 05:41:34 -0500

    Started limelight production.  Altered spec_helper to point directly to the limelight spec_helper since it is running off a jarred jruby.

---
+++ gameoflife/stages.rb
@@ -0,0 +1,17 @@
+# This file (stages.rb) is used to define the stages within your production.
+#
+# Below is an example statge configuration.
+#
+#  stage "center_stage" do
+#    default_scene "main"
+#    title "Limelight Center Stage"
+#    location [0, 0]
+#    size [300, 800]
+#  end
+
+stage "gof" do
+  default_scene "default_scene"
+  title "Game Of Life"
+  location [200, 25]
+  size [400, 400]
+end
